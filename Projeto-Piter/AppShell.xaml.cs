09a2fc3e80c6c9ef4d15d0e09379352608a96433
PTLS7 <piter.siqueira@immes.edu.br>
2025-04-14 13:08:46 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto-Piter/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Projeto_Piter
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Projeto-Piter/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Projeto_Piter
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
